43639c509599259bef2c3b438dac1f67798bdf18
gastongomezcuello <gastongomezcuello@gmail.com>
2024-11-11 14:55:36 -0300

    profile-model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+
+    CHOICE = (
+        ("full-time", "full-time"),
+        ("part-time", "part-time"),
+        ("freelance", "freelance"),
+        ("other", "other"),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    phone_number = models.CharField(max_length=24, blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures", blank=True, null=True
+    )
+    cover_picture = models.ImageField(upload_to="cover_pictures", blank=True, null=True)
+    occupation = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    availability = models.CharField(
+        max_length=100, choices=CHOICE, blank=True, null=True
+    )
+    birth_date = models.DateField(blank=True, null=True)
+    years_experience = models.PositiveIntegerField(blank=True, null=True)
+    address = models.CharField(max_length=150, blank=True, null=True)
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+
+    # language
+    # country
+
+    def __str__(self):
+        return self.user.username
