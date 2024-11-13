d0dd64d653d88c865d411defaecc62fe8dc5adb9
gastongomezcuello <gastongomezcuello@gmail.com>
2024-11-13 11:16:49 -0300

    Coin-&-Transaction-models

--- coins/admin.py
+++ coins/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from coins.models import Card
+from coins.models import Card, Coin, Transaction
 
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     list_display = ("card_name",)
+
+
+@admin.register(Coin)
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Transaction)
+class Transaction(admin.ModelAdmin):
+    list_display = ("coin", "transaction_type")
--- coins/models.py
+++ coins/models.py
@@ -26,3 +26,30 @@ class Card(models.Model):
 
     def __str__(self):
         return self.card_name
+
+
+class Coin(models.Model):
+
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    description = models.TextField()
+    image = models.ImageField(upload_to="coins")
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+
+    CHOICE = (("BUY", "BUY"), ("SELL", "SELL"))
+
+    coin = models.ForeignKey(
+        Coin, on_delete=models.CASCADE, related_name="transactions"
+    )
+    price = models.FloatField()
+    amount = models.FloatField()
+    date = models.DateTimeField()
+    transaction_type = models.CharField(max_length=4, choices=CHOICE)
+
+    def __str__(self):
+        return f"{self.coin.name} - {self.transaction_type}"
